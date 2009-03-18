ca43b7a4a3f6b9bcb433987de45f960e68061ddc
Adam Keys <adam@therealadam.com>
2009-03-18 12:01:51 -0500

    Now cache friendly!
    * Store callbacks for each unique app/metric/window combination.
    * The JSON-P returned by the server will always call $.dashRouter
    * $.dashRouter dispatches to any callbacks matching the app/metric/window combination the server just sent

--- jquery.dash.js
+++ jquery.dash.js
@@ -1,21 +1,27 @@
 (function($) {
+  
+  var dashCache = {};
+  var dashListeners = {};
+  
   var defaults = function() {
     return {
         token: '',
         metric: '',
-        dashHost: 'http://dash-api.fiveruns.com'
+        window: 0,
+        // dashHost: 'http://dash-api.fiveruns.com'
+        dashHost: 'http://localhost:3000'
     };
   };
   
   var apiUrl = function(options) {
     return options.dashHost + 
            '/apps/' + 
-           options.token + 
-           '/data-v1.js?callback=?';
+           options.token +
+           '/data-v1.js';
   };
   
   var extractParams = function(options) {
-    params = {};
+    params = {callback: '$.dashRouter'};
     
     for (name in options) {
       switch (name) {
@@ -33,29 +39,58 @@
     return params;
   };
   
+  var requestKey = function(token, metric, window) {
+    return token + "-" + metric + "-" + window;
+  };
+  
+  var addListener = function(params, callback) {
+    var key = requestKey(params.token, params.metric, params.window);
+    if (dashListeners[key] != null) {
+      dashListeners[key].push(callback);
+    } else {
+      dashListeners[key] = [callback];
+    }
+  };
+  
+  $.dashRouter = function(obj) {
+    var key = requestKey(obj.token, obj.metricName, obj.window);
+    $.each(dashListeners[key], function(_, callback) { callback(obj); });
+  };
+  
   $.fn.dash = function(options, callback) {
     var options = $.extend(defaults(), options ? options : {});
     var el = this;
+    var params = extractParams(options);
+    
+    var handler = function(obj) {
+      console.log('borp');
+      switch (options.fetch) {
+        case "all":
+          callback.apply(el, [obj]);
+          break;
+        case "series":
+          var datum = $.map(obj.data, function(i) { return i[1]; });
+          callback.apply(el, [datum]);
+          break;
+        case "latest":
+          var value = obj.data.reverse().pop().pop();
+          callback.apply(el, [value]);
+          break;
+        default:
+          throw "Invalid Dash fetch option";
+          break;
+      };
+    };
+    
+    addListener(options, handler);
     
     this.each(function() {
-      $.getJSON(apiUrl(options), extractParams(options), function(obj) {
-        switch (options.fetch) {
-          case "all":
-            callback.apply(el, [obj]);
-            break;
-          case "series":
-            var datum = $.map(obj.data, function(i) { return i[1]; });
-            callback.apply(el, [datum]);
-            break;
-          case "latest":
-            var value = obj.data.reverse().pop().pop();
-            callback.apply(el, [value]);
-            break;
-          default:
-            throw "Invalid Dash fetch option";
-            break;
-        };
-      });
+      $.ajax({type: 'GET',
+              url: apiUrl(options),
+              data: params,
+              dataType: 'script',
+              // success: handler,
+              cache: true});
     });
     
     return this;
